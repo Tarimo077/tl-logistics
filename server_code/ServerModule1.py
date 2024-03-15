79868e57d3bd75b6024f7fe4def2a062f6b88647
givelimited@gmail.com <givelimited@gmail.com>
2024-03-15 22:15:25 +0000

    Created ServerModule1, Form1.RowTemplate2, Form1.RowTemplate1 and custom package dependencies; Edited settings and Form1

---
+++ server_code/ServerModule1.py
@@ -0,0 +1,36 @@
+import anvil.server
+import anvil.http
+import anvil.media
+import locale
+from collections import defaultdict
+
+# This is a server module. It runs on the Anvil server,
+# rather than in the user's browser.
+#
+# To allow anvil.server.call() to call functions here, we mark
+# them with @anvil.server.callable.
+# Here is an example - you can replace it with your own:
+#
+# @anvil.server.callable
+# def say_hello(name):
+#   print("Hello, " + name + "!")
+#   return 42
+#
+
+@anvil.server.callable
+def getDrivers():
+  url = "https://appliapay.com/getDrivers"
+  headers = {
+    "Content-Type": "application/json"
+  }
+  response = anvil.http.request(url, method="GET", headers=headers, username='admin', password='123Give!@#')
+  return response
+
+@anvil.server.callable
+def getTrips():
+  url = "https://appliapay.com/getTrips"
+  headers = {
+    "Content-Type": "application/json"
+  }
+  response = anvil.http.request(url, method="GET", headers=headers, username='admin', password='123Give!@#')
+  return response
