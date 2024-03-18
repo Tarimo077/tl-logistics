915a83f7db5103400d3ba8aaaaa9085d78d3a9dd
givelimited@gmail.com <givelimited@gmail.com>
2024-03-18 07:01:14 +0000

    Edited Login

--- client_code/Login/__init__.py
+++ client_code/Login/__init__.py
@@ -12,7 +12,7 @@ class Login(LoginTemplate):
     #dom_node.style.background = f"url(_/theme/TLbackground.jfif)"
     body_tag = anvil.js.window.document.body
     self.image_1.background = None
-    body_tag.style.background =  "#21005E"#f"url(_/theme/TLbackground.jfif)"
+    body_tag.style.background =  "#6750A4"#f"url(_/theme/TLbackground.jfif)"
     self.eye.tooltip = 'view password'
     self.pass_state = False
     slide_in_up = Transition(translateY=["100%", 0])
@@ -24,3 +24,21 @@ class Login(LoginTemplate):
     self.password.hide_text = True
 
     # Any code you write here will run before the form opens.
+
+  def eye_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    if self.pass_state == False:
+      self.password.hide_text = False
+      self.eye.icon = 'fa:eye-slash'
+      self.pass_state = True
+      self.eye.foreground = '#FFFFFF'
+      self.eye.background = '#FF0000'
+      self.eye.tooltip = 'hide password'
+      
+    else:
+      self.password.hide_text = True
+      self.eye.icon = 'fa:eye'
+      self.pass_state = False
+      self.eye.background = '#FFFFFF'
+      self.eye.foreground = '#21005E'
+      self.eye.tooltip = 'view password'
