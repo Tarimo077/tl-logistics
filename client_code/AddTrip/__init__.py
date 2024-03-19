0909900440c551e53bd04c9b53bfb89a705c8c44
givelimited@gmail.com <givelimited@gmail.com>
2024-03-19 10:57:33 +0000

    Created new_file.js; Edited settings, AddTrip and standard-page.html

--- client_code/AddTrip/__init__.py
+++ client_code/AddTrip/__init__.py
@@ -4,6 +4,8 @@ import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+import json
+from anvil_extras.animation import animate, get_bounding_rect
 
 class AddTrip(AddTripTemplate):
   def __init__(self, **properties):
@@ -11,3 +13,14 @@ class AddTrip(AddTripTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def newDriver_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    bounding_rect = get_bounding_rect(self.dropdownDriver)
+        
+        # Create and position the new component using the bounding rect
+    new_component = TextBox(text="New Component")
+    print(bounding_rect.x)
+    new_component.y = bounding_rect.y
+    self.dropdownDriver.remove_from_parent()
+    self.add_component(new_component, x=bounding_rect.x, y=bounding_rect.y, width=bounding_rect.width, height=bounding_rect.height)
