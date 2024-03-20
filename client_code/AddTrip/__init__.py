ac2d10858b2285131af9166c50c7fb8e32c0ad7d
givelimited@gmail.com <givelimited@gmail.com>
2024-03-20 17:16:26 +0000

    Edited theme.css and AddTrip; Deleted Form1 and new_file.js

--- client_code/AddTrip/__init__.py
+++ client_code/AddTrip/__init__.py
@@ -7,11 +7,13 @@ from anvil.tables import app_tables
 import json
 from anvil_extras.animation import animate, get_bounding_rect
 from time import sleep
+from anvil_extras import MessagePill
 
 class AddTrip(AddTripTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.msgPill.visible = False
     response = anvil.server.call('getDrivers')
     text = response.get_bytes().decode('utf-8')
     drivers = json.loads(text)
@@ -32,3 +34,15 @@ class AddTrip(AddTripTemplate):
   def addTrip_click(self, **event_args):
     """This method is called when the button is clicked"""
     if self.dropdownDriver.selected_value is None:
+      self.msgPill.message = "CHOOSE DRIVER"
+      self.msgPill.visible = True
+      #alert('Choose driver', buttons=None)
+      return
+    if self.amount.text is None or self.txtFrom.text is None or self.txtTo.text is None or self.date.date is None:
+      self.msgPill.message = "ENTER ALL FIELDS"
+      self.msgPill.visible = True
+      return
+    if self.amount.text is not None or self.dropdownDriver.selected_value is not None or self.txtFrom.text is not None or self.txtTo.text is not None or self.date.date is not None:
+      self.msgPill.visible = False
+      amnt = "${:,.0f}".format(self.amount.text)
+      print(str(amnt))
